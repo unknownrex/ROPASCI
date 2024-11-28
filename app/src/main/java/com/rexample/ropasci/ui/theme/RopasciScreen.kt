3199c6810d985371218f54d0d3593c2264c91bfc
Rex <reyvanfarrel@gmail.com>
2024-11-28 17:43:07 +0700

    Init

---
+++ app/src/main/java/com/rexample/ropasci/ui/theme/RopasciScreen.kt
@@ -0,0 +1,25 @@
+package com.rexample.ropasci.ui.theme
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RopasciScreen() {
+
+}
+
+@Composable
+fun ScoreCard() {
+
+}
+
+@Composable
+fun ChoiceButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column {
+
+    }
+}
